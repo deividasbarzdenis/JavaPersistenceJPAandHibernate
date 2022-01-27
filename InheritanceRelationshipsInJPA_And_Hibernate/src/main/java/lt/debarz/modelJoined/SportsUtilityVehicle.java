6476a60b9cb965b07fc2057f9b7a632f258e85ad
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-01-27 21:48:35 +0200

    Added example with Inheritance relationships in JPA and Hibernate

---
+++ InheritanceRelationshipsInJPA_And_Hibernate/src/main/java/lt/debarz/modelJoined/SportsUtilityVehicle.java
@@ -0,0 +1,18 @@
+package main.java.lt.debarz.modelJoined;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "SUV")
+public class SportsUtilityVehicle extends Car {
+    private Integer towingCapacity;
+    private Boolean thirdRowSeating;
+}
